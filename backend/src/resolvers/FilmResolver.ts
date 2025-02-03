6458bd2fa55ffac1519b3990aa9aadaf949e9255
grafuj <sfrrki@gmail.com>
2025-02-03 12:08:07 -0800

    add tsconfig decorator settings, update getRepository to AppDataSource.getRepository in film and review

--- backend/src/resolvers/FilmResolver.ts
+++ backend/src/resolvers/FilmResolver.ts
@@ -1,26 +1,24 @@
-import { Resolver, Query, Mutation, Arg } from 'type-graphql';
-import { Film } from '../entities/Film';
-import { getRepository } from 'typeorm';
+import 'reflect-metadata';
+import { Resolver, Query, Mutation, Arg } from "type-graphql";
+import { Film } from "../entities/Film";
+import { AppDataSource } from "../data-source";
 
 @Resolver(Film)
 export class FilmResolver {
   @Query(() => [Film])
   async films(): Promise<Film[]> {
-    return getRepository(Film).find();
+    return AppDataSource.getRepository(Film).find();
   }
 
   @Mutation(() => Film)
   async addFilm(
-    @Arg('name') name: string,
-    @Arg('releaseDate') releaseDate: string,
-    @Arg('imdbUrl') imdbUrl: string,
-    @Arg('genre') genre: string
+    @Arg("name") name: string,
+    @Arg("releaseDate") releaseDate: string,
+    @Arg("imdbUrl") imdbUrl: string,
+    @Arg("genre") genre: string
   ): Promise<Film> {
-    const film = new Film();
-    film.name = name;
-    film.releaseDate = releaseDate;
-    film.imdbUrl = imdbUrl;
-    film.genre = genre;
-    return getRepository(Film).save(film);
+    const filmRepository = AppDataSource.getRepository(Film);
+    const film = filmRepository.create({ name, releaseDate, imdbUrl, genre });
+    return filmRepository.save(film);
   }
 }
--- backend/src/resolvers/ReviewResolver.ts
+++ backend/src/resolvers/ReviewResolver.ts
@@ -1,26 +1,23 @@
-import { Resolver, Query, Mutation, Arg } from 'type-graphql';
-import { Review } from '../entities/Review';
-import { getRepository } from 'typeorm';
+import { Resolver, Query, Mutation, Arg } from "type-graphql";
+import { Review } from "../entities/Review";
+import { AppDataSource } from "../data-source";
 
 @Resolver(Review)
 export class ReviewResolver {
   @Query(() => [Review])
   async reviews(): Promise<Review[]> {
-    return getRepository(Review).find();
+    return AppDataSource.getRepository(Review).find();
   }
 
   @Mutation(() => Review)
   async addReview(
-    @Arg('filmId') filmId: string,
-    @Arg('userId') userId: string,
-    @Arg('engagementScore') engagementScore: number
+    @Arg("filmId") filmId: string,
+    @Arg("userId") userId: string,
+    @Arg("engagementScore") engagementScore: number,
     // Add other fields...
   ): Promise<Review> {
-    const review = new Review();
-    review.filmId = filmId;
-    review.userId = userId;
-    review.engagementScore = engagementScore;
-    // Set other fields...
-    return getRepository(Review).save(review);
+    const reviewRepository = AppDataSource.getRepository(Review);
+    const review = reviewRepository.create({ filmId, userId, engagementScore });
+    return reviewRepository.save(review);
   }
 }
