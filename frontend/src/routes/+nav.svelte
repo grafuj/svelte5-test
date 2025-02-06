dcb3631267c37b8a2a29c270272b3e7384f80913
grafuj <sfrrki@gmail.com>
2025-02-06 14:51:17 -0800

    add nav bar that will hopefully render on all pages

---
+++ frontend/src/routes/+nav.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	let { children } = $props();
+</script>
+
+<nav>
+	<a href="/">Home</a>
+	<a href="/about">About</a>
+	<a href="/films">Films</a>
+	<a href="/users">Users</a>
+	<a href="/reviews">Reviews</a>
+	<a href="/settings">Settings</a>
+</nav>
+
+{@render children()}
